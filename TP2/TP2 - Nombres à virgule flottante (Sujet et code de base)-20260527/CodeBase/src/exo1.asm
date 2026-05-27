92dfdf1be04f00c7912f1741f19bf62059935c9d
bakame03 <nganjialdoalex@gmail.com>
2026-05-27 10:57:09 +0200

    feat: remove unused legacy data migration module

--- TP2/TP2 - Nombres à virgule flottante (Sujet et code de base)-20260527/CodeBase/src/exo1.asm
+++ TP2/TP2 - Nombres à virgule flottante (Sujet et code de base)-20260527/CodeBase/src/exo1.asm
@@ -6,7 +6,7 @@
 	STDOUT    equ 1
 	SUCCES    equ 0
 
-extern lireCaractere
+extern lireCaractere	
 extern afficherCaractere	
 extern afficherString
 
@@ -22,6 +22,21 @@ section .text
 	global main
 main:	
 	;; Ajoutez votre code ici.
+	mov rdi, msgLettre
+    call afficherString
+	call lireCaractere
+	mov [carac], al
+
+	;; ------------------------
+	mov edi, [carac]            
+    call majuscule          
+    mov [majus], al
+    ;; ------------------------
+	mov rdi, msgMajuscule   
+    call afficherString
+
+	mov edi, [majus]        
+    call afficherCaractere
 	
 Fin :
        ; Code de finalisation du programme.
@@ -32,5 +47,6 @@ Fin :
 
 majuscule:	
 	;; Ajoutez votre code ici.
-
-	ret
+	mov eax, edi         
+	sub eax, 32             
+    ret
